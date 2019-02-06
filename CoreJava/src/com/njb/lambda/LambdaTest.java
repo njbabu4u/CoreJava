d1096481cfa943ec297512dd11fce847d634ba30
Babu N J <njbabu4u@gmail.com>
2019-02-06 21:33:28 +0530

    lambda - send of 06-Feb-2019

--- CoreJava/src/com/njb/lambda/LambdaTest.java
+++ CoreJava/src/com/njb/lambda/LambdaTest.java
@@ -9,16 +9,68 @@ import jdk.nashorn.internal.ir.Block;
 
 public class LambdaTest {
 	public static void main(String[] args){
-		test1();
-		test2();
+		forEachTest();
+		streamTest();
 	}
 
-	private static void test2() {
-		// TODO Auto-generated method stub
+	private static void streamTest() {
 		
+	    List<String> someBingoNumbers = Arrays.asList(
+	            "N40", "N36",
+                "B12", "B6",
+                "G53", "G49", "G60", "G50", "g64",
+                "I26", "I17", "I29",
+                "O71");
+		
+        List<String> gNumbers = new ArrayList<>();
+        
+/*
+		someBingoNumbers.forEach(number -> {
+			if (number.toUpperCase().startsWith("G")) {
+				gNumbers.add(number);
+				System.out.println(number);
+			}
+		});
+		
+		System.out.println("----------------");
+		
+		gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+		gNumbers.forEach((String s) -> System.out.println(s));
+		System.out.println("----------------");
+*/
+        
+        
+/*        
+        someBingoNumbers
+        .stream()
+        .map(String::toUpperCase)
+        .filter(s->s.startsWith("G"))
+        .sorted()
+        .forEach(System.out::println);
+        System.out.println("----------------");
+*/        
+        
+        someBingoNumbers
+        .stream()
+        .map(String::toUpperCase)
+        .filter(s->s.startsWith("G"))
+        .sorted()
+        .forEach(s -> { System.out.println(s); gNumbers.add(s); });
+        System.out.println("----------------");
+        
+        gNumbers.forEach(System.out::println);
+        System.out.println("----------------");
+        
+        gNumbers.forEach(s->printMe(s));
+        System.out.println("----------------");
+	
+	}
+	
+	private static void printMe(String s) {
+		System.out.println(s);
 	}
 
-	private static void test1() {
+	private static void forEachTest() {
 		List<String> myList = Arrays.asList("H","E","L","L","O");
 		myList.forEach(s -> System.out.println(s));
 		myList.forEach(s -> { System.out.println(s); System.out.println("Jagadeesh-"+s);});
